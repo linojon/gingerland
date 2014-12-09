79c3281727ee39763dfa98cf608ddd8199691dba
Jonathan Linowes <jonathan@parkerhill.com>
2014-12-08 22:23:13 -0500

    exit holes work

--- Assets/scripts/ExitHole.cs
+++ Assets/scripts/ExitHole.cs
@@ -4,34 +4,30 @@ using System.Collections;
 public class ExitHole : MonoBehaviour {
 
 	private bool  isExiting			= false;
+	private float distance;
+	private GameObject exitHole;
+	private GameObject player;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		exitHole = GameObject.FindGameObjectWithTag("exitHole");
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-//	void OnCollisionEnter(Collision col) {
-//		if (col.gameObject.CompareTag ("Player")) {
-//			Debug.Log ("Hit the Hole!");
-//		}
-//	}
-	void OnTriggerEnter(Collider other) {
-		if (!isExiting && other.CompareTag ("Player")) {
-			print ("Hit the Hole!");
-
-//			GameObject playerObj = other.gameObject;
-//			Rigidbody rigid = playerObj.AddComponent<Rigidbody>();
-//			rigid.useGravity = true;
-			isExiting = true;
-
-			// wait for player to fall or something, then
-			Application.LoadLevel("SleighLand");
+		distance = Vector3.Distance( exitHole.transform.position, player.transform.position );
+		if (distance < 2) {
+			ExitGingerLand();
 		}
+		Debug.Log (distance);
 	}
+
+	private void ExitGingerLand() {
+		isExiting = true;
+		// wait for player to fall or something, then
+		Application.LoadLevel("SleighLand");
+	}
+
 }
