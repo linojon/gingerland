93e181d132dcfe5cc4dd983994248eeb2d6e2bc7
Jonathan Linowes <jonathan@parkerhill.com>
2014-12-08 21:26:27 -0500

    helpText

---
+++ Assets/scripts/HelpText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpText : MonoBehaviour {
+
+	private GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		text = GameObject.FindGameObjectWithTag("helpText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) {
+			Debug.Log ("key pressed!!");
+			text.SetActive(false);
+		}
+	}
+}
