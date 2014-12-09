72ce4936f97d135300859db049b3bbd8e56995bf
Jonathan Linowes <jonathan@parkerhill.com>
2014-12-09 00:16:39 -0500

    falling hole with mario clip

--- Assets/scripts/ExitHole.cs
+++ Assets/scripts/ExitHole.cs
@@ -5,24 +5,26 @@ public class ExitHole : MonoBehaviour {
 
 	private bool  isExiting			= false;
 	private float distance;
-	private GameObject exitHole;
+	private GameObject[] exitHoles;
 	private GameObject player;
 
 
 	// Use this for initialization
 	void Start () {
-		exitHole = GameObject.FindGameObjectWithTag("exitHole");
+		exitHoles = GameObject.FindGameObjectsWithTag("exitHole");
 		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!isExiting) {
-			distance = Vector3.Distance( exitHole.transform.position, player.transform.position );
-			if (distance < 2) {
-				ExitGingerLand();
+			foreach(GameObject exitHole in exitHoles) {
+				distance = Vector3.Distance( exitHole.transform.position, player.transform.position );
+				if (distance < 2) {
+					ExitGingerLand();
+				}
+//				Debug.Log (distance);
 			}
-//			Debug.Log (distance);
 		}
 	}
 
@@ -34,10 +36,13 @@ public class ExitHole : MonoBehaviour {
 		player.GetComponent<FPSInputController>().enabled = false;
 
 		Rigidbody body = player.AddComponent<Rigidbody>(); // Add the rigidbody.
+//		yield return new WaitForSeconds(1);
+		// this sound clip is 16hz delay n seconds
+		audio.Play(16000 * 3);
+//		yield return new WaitForSeconds(10);
 
-	
 		// wait for player to fall or something, then
-//		Application.LoadLevel("SleighLand");
+		//Application.LoadLevel("SleighLand");
 	}
 
 }
