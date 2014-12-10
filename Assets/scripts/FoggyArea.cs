b34f5f3089bbdb3d74b69408d23d6f46ffebe1f9
Jonathan Linowes <jonathan@parkerhill.com>
2014-12-10 00:05:36 -0500

    enable ghosts by distance

--- Assets/scripts/FoggyArea.cs
+++ Assets/scripts/FoggyArea.cs
@@ -3,25 +3,26 @@ using System.Collections;
 
 public class FoggyArea : MonoBehaviour {
 
-	public GameObject evilObject;
 	public float effectDistance;
 	public float maxFogDensity;
 
-	private bool  	startFogEnable;
-	private float 	startFogDensity; 
-	private FogMode startFogMode;
+	private GameObject 	player;
+	private bool  		startFogEnable;
+	private float 		startFogDensity; 
+	private FogMode 	startFogMode;
 
 	private float distance, percent;
 
 	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
 		startFogEnable 	= RenderSettings.fog;
 		startFogDensity = RenderSettings.fogDensity;
 		startFogMode 	= RenderSettings.fogMode;
 	}
 	
 	void Update () {
-		distance = Vector3.Distance( evilObject.transform.position, gameObject.transform.position );
-		if (distance < 0) { distance = -distance; }
+		distance = Vector3.Distance( gameObject.transform.position, player.transform.position );
+//		if (distance < 0) { distance = -distance; }
 		//Debug.Log (distance);
 
 		if (distance <= effectDistance) {
